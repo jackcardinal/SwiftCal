3ffe252d2f9e70197f04d014fda1dcb2d373f819
Jack Cardinal <general@jackcardinal.com>
2023-03-15 12:31:53 -0700

    Initial commit, right before I we fix the calendar

---
+++ SwiftCal/Persistence.swift
@@ -0,0 +1,46 @@
+//
+//  Persistence.swift
+//  SwiftCal
+//
+//  Created by Jack Cardinal on 3/9/23.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    static var preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        let startDate = Calendar.current.dateInterval(of: .month, for: .now)!.start
+        for dateOffSet in 0..<30 {
+            let newDay = Day(context: viewContext)
+            newDay.date = Calendar.current.date(byAdding: .day, value: dateOffSet, to: startDate)
+            newDay.didStudy = Bool.random()
+        }
+        do {
+            try viewContext.save()
+        } catch {
+
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        return result
+    }()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "SwiftCal")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
